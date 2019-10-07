f9a5ae49981351a9917eb9f39bcb372f3003a8d6
andoa-infra <ando.shoya.infratop@gmail.com>
2019-10-07 12:06:11 +0900

    made validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,12 @@ class BooksController < ApplicationController
   def index
   	@books = Book.page(params[:page]).reverse_order
     @book=Book.new
+
   end
 
   def show
   	@book = Book.find(params[:id])
+    @book = Book.new
   end
 
   def new
@@ -16,15 +18,35 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
     @book.user_id = current_user.id
+    @books = Book.all
   	if @book.save
       flash[:notice]="book was successfully created"
-  	  redirect_to book_path(@book.id)
+  	  redirect_to book_path(@book)
     else
-      render :new
+      render 'index'
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to books_path(@book.id)
+      flash[:notice]="book was successfully updated"
+    else
+      flash[:notice]="book was not updated"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'root#top'
 
-  resources :users, only: [ :index, :show, :edit, :update]
+  resources :users, only: [ :index, :show, :edit, :update, :destroy]
   resources :books
 end
 
